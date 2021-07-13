19a277f2b3e8aa19bb5547b88c9d375703d3d596
herlianali <aliyasa58@gmail.com>
2021-07-13 15:48:59 +0700

    pertama

---
+++ app.js
@@ -0,0 +1,52 @@
+const { Client } = require('whatsapp-web.js');
+const express = require('express');
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+
+const app = express();
+
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: true,
+        message: "Hello-world!"
+    });
+});
+
+const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    // qrcode.generate(qr);
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg=session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
+
+app.listen(8000, function() {
+    console.log("App runing on *: " + 8000);
+});
